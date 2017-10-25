792fbde5cab1696edbed93d2b5151c5ccbf7c928
khangvtran <vinhkhangtran09@gmail.com>
2017-10-24 17:19:27 -0700

    readChart() and printChart() finished

--- lab5/Lab5.py
+++ lab5/Lab5.py
@@ -1,3 +1,7 @@
+# Author: Khang V. Tran, Lacee Xu
+# Lab5
+
+
 """
 a = []
 a1 = "a b c"
@@ -8,32 +12,64 @@ for i in range(len(a)) : print(a[i].split())
 
 print(len("  $10"))
 print("%5s" %"$10")
-"""
+
 #print(len("Row  ========================================"))
 #print(len("Row  "))
-
-fileName = input("Enter a file name or hit Enter to use lab5input2.txt: ")
-if fileName =="" : fileName = "lab5input2.txt"
-seatingChart = []
-with open(fileName, "r") as infile:
-    for line in infile:
-        seatingChart.append(line.split())
 
 #for i in range(len(seatingChart)) : print(seatingChart[i])
 #print(seatingChart[1])
 #print(seatingChart[1][1])
 #print(len(seatingChart[1]))
 
-numRow = len(seatingChart)
-numCol = len(seatingChart[0])
-print("%24s" %"Column")                # print 'Column'
-print("Row  " +  "="*5*numCol)         # print Row ==================
-print(" "*5, end = "")
-for i in range(0, numCol) : print( "%5s"  %str(i+1), end = "")
-print("\n")
-for row in range(numRow):
-    print(str(row+1) + "  | ", end = "") 
-    for col in range(numCol):
-        print("%5s"  %str("$" + seatingChart[row][col]), end = "")
+fileName = input("Enter a file name or hit Enter to use lab5input2.txt: ")
+try:
+    with open(fileName, "r") as infile:
+        print("abc")
+except FileNotFoundError as e:
+    fileName = input("Enter a file name or hit Enter to use lab5input2.txt: ")
+"""
+
+
+
+
+
+
+"""
+readChart prompt user to enter file name
+if file doesn't exist, handle exception and prompt again 
+if user enters, use lab5input.txt as default
+call printchart() to print the seating chart
+"""
+def readChart():
+    try:
+        fileName = input("Enter a file name or hit Enter to use lab5input2.txt: ")
+        if fileName == "" : fileName = "lab5input2.txt"
+        seatingChart = []
+        with open(fileName, "r") as infile:
+            for line in infile:
+                seatingChart.append(line.split())
+        printChart(seatingChart)           # call printChart() function
+    except FileNotFoundError as e:       
+        print("Can't open" + str(e))
+        readChart()
+        
+"""
+print the seating chart in the desired format
+"""
+def printChart(seatingChart):
+    numRow = len(seatingChart)
+    numCol = len(seatingChart[0])
+    print("Price Chart".center((numCol+1)*5))
+    print("Column".center((numCol+1)*5))             # print 'Column'
+    print("Row  " +  "="*5*numCol)         # print Row ==================
+    print(" "*5, end = "")                   
+    for i in range(0, numCol) : print( "%5s"  %str(i+1), end = "")     
     print("\n")
-    
+    for row in range(numRow):
+        print(str(row+1) + "  | ", end = "") 
+        for col in range(numCol):
+            print("%5s"  %str("$" + seatingChart[row][col]), end = "")
+        print("\n")
+
+### Driver Code
+readChart()
